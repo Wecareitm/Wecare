7cc8c2c13669bc1a2e71a4aa9ac6d109f632f0bb
Wecareitm <Wecareitm@gmail.com>
2020-01-03 10:56:47 +0530

    servicepersonal volley(mit)

---
+++ app/src/main/java/com/example/wecare/ServicePersonFrogotPassword.java
@@ -0,0 +1,67 @@
+package com.example.wecare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.wecare.utils.Commonfunction;
+import com.example.wecare.utils.Constants;
+import com.example.wecare.utils.DataInterface;
+import com.example.wecare.utils.Webservice_Volley;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+public class ServicePersonFrogotPassword extends AppCompatActivity implements DataInterface {
+    TextView txt_forgotpass,txt_submit;
+
+    EditText edt_email;
+    Button btn_Submit;
+
+    Webservice_Volley volley;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service_person_frogot_password);
+        edt_email = (EditText)findViewById(R.id.edt_email);
+        btn_Submit = (Button)findViewById(R.id.btn_submit);
+        volley = new Webservice_Volley(this,this);
+
+        btn_Submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (!Commonfunction.checkString(edt_email.getText().toString())){
+                    edt_email.setError("Please enter your email");
+                    edt_email.requestFocus();
+                    return;
+
+                }
+
+
+                String url = Constants.Webserive_Url + "sp_forgotpsw.php";
+
+                HashMap<String,String> params = new HashMap<>();
+                params.put("u_email",edt_email.getText().toString());
+                volley.CallVolley(url,params,"sp_forgotpsw");
+
+
+            }
+        });
+
+    }
+
+    @Override
+    public void getData(JSONObject jsonObject, String tag) {
+        Toast.makeText(this, jsonObject.toString(), Toast.LENGTH_SHORT).show();
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/wecare/ServicePersonResetPassword.java
@@ -0,0 +1,78 @@
+package com.example.wecare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.wecare.utils.Commonfunction;
+import com.example.wecare.utils.Constants;
+import com.example.wecare.utils.DataInterface;
+import com.example.wecare.utils.Webservice_Volley;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+public class ServicePersonResetPassword extends AppCompatActivity implements DataInterface {
+    EditText edt_newpassword,edt_confirmnewpassword;
+    TextView txt_resetpassword;
+    Button btn_reset;
+
+    String userid="";
+
+    Webservice_Volley volley;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service_person_reset_password);
+        edt_newpassword = (EditText)findViewById(R.id.edt_email_mobile);
+        edt_confirmnewpassword = (EditText)findViewById(R.id.edt_password);
+
+        txt_resetpassword=(TextView)findViewById(R.id.txt_forgotpassword);
+        btn_reset = (Button)findViewById(R.id.btn_login);
+
+
+        volley = new Webservice_Volley(this,this);
+
+        btn_reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (!Commonfunction.checkString(edt_newpassword.getText().toString())){
+                    edt_newpassword.setError("Please enter new password");
+                    edt_newpassword.requestFocus();
+                    return;
+
+                }
+
+                if (!Commonfunction.checkPassword(edt_confirmnewpassword.getText().toString())){
+                    edt_confirmnewpassword.setError("Please confirm new password");
+                    edt_confirmnewpassword.requestFocus();
+                    return;
+                }
+
+
+                String url = Constants.Webserive_Url + "sp_resetpsw.php";
+
+                HashMap<String,String> params = new HashMap<>();
+                params.put("sp_password",edt_newpassword.getText().toString());
+                params.put("sp_id",userid);
+
+                volley.CallVolley(url,params,"sp_resetpsw");
+
+
+            }
+        });
+    }
+
+    @Override
+    public void getData(JSONObject jsonObject, String tag) {
+        Toast.makeText(this, jsonObject.toString(), Toast.LENGTH_SHORT).show();
+
+    }
+}
