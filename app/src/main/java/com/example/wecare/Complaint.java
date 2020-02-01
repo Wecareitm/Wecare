38d23cfe977a50f5cd4887c7b2b1d85e5ccc079f
wecareitm <wecareitm@gmail.com>
2020-02-01 12:42:55 +0530

    profile and complaint(pratik11)

--- app/src/main/java/com/example/wecare/Complaint.java
+++ app/src/main/java/com/example/wecare/Complaint.java
@@ -3,12 +3,33 @@ package com.example.wecare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.toolbox.Volley;
+import com.example.wecare.utils.Webservice_Volley;
 
 public class Complaint extends AppCompatActivity {
+
+    TextView txt_complaint;
+    EditText edit_Complaint;
+    Button  btn_PostComplaint;
+    Webservice_Volley volley;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_complaint);
+
+        txt_complaint =(TextView)findViewById(R.id.txt_complaint);
+        edit_Complaint=(EditText)findViewById(R.id.edit_Complaint);
+        btn_PostComplaint=(Button)findViewById(R.id.btn_PostComplaint);
+
+        volley  = new Webservice_Volley(this,this);
+
+
+
     }
 }
--- app/src/main/java/com/example/wecare/Servicepersonlogin.java
+++ app/src/main/java/com/example/wecare/Servicepersonlogin.java
@@ -59,7 +59,7 @@ public class Servicepersonlogin extends AppCompatActivity implements DataInterfa
 
 
                 HashMap<String,String> params = new HashMap<>();
-                params.put("sp_email",edt_email_mobile.getText().toString());
+                params.put("sp_mobile",edt_email_mobile.getText().toString());
                 params.put("sp_password",edt_password.getText().toString());
 
                 volley.CallVolley(url,params,"sp_login");
