9c8619bed9d8b1517025679463c661a43c88f24a
Wecareitm <wecareitm@gmail.com>
2020-02-06 12:06:57 +0530

    navigation   icon yash

--- app/src/main/java/com/example/wecare/SplashActivity.java
+++ app/src/main/java/com/example/wecare/SplashActivity.java
@@ -26,7 +26,7 @@ public class SplashActivity extends AppCompatActivity {
             public void run() {
                 // This method will be executed once the timer is over
                 // Start your app main activity
-                Intent i = new Intent(SplashActivity.this, Userselection.class);
+                Intent i = new Intent(SplashActivity.this, UserHomepageActivity.class);
                 startActivity(i);
 
                 // close this activity
